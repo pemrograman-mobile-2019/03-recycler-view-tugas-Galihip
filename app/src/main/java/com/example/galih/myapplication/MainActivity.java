70aa8edb3f988ac8d19d6e9fc83d5162eff58cca
Galihip <galih.mlg0699@gmail.com>
2019-02-27 22:00:46 +0700

    recycler-view-tugas

--- app/src/main/java/com/example/galih/myapplication/MainActivity.java
+++ app/src/main/java/com/example/galih/myapplication/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.galih.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.galih.myapplication.adapter.ContactsAdapter;
+import com.example.galih.myapplication.models.Contact;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<Contact> contacts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rv = findViewById(R.id.rvContacts);
+        contacts = Contact.createContactsList(10);
+        ContactsAdapter contactsAdapter= new ContactsAdapter(contacts);
+        RecyclerView.LayoutManager layoutManager= new LinearLayoutManager( this);
+        rv.setAdapter(contactsAdapter);
+        rv.setLayoutManager(layoutManager);
+
+
+
     }
 }
